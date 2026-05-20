b72b9ad8cf681564c76152905635bc2b7b4e453f
Will Sinatra <durrendal@lambdacreate.com>
2026-05-19 21:35:05 -0400

    Revert "Update dependency https://github.com/salt-extensions/salt-extension-copier to v0.9.0"

--- docs/conf.py
+++ docs/conf.py
@@ -73,7 +73,9 @@ with open("sitevars.rst") as site_vars_file:
 
 rst_prolog = """
 {}
-""".format("\n".join(site_vars[:]))
+""".format(
+    "\n".join(site_vars[:])
+)
 
 # -- General configuration ---------------------------------------------------
 
